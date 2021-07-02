65bc0d4e70be956596850c0a4b0bf7de809de219
linad577 <84777339+linad577@users.noreply.github.com>
2021-07-02 19:47:17 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,21 @@
+#!/xbin/python/python2
+
+
+
+
+print ("[✓] Author : linad577")
+
+
+print ('penjumlahan')
+
+
+x = input('masukkan angka ke 1 :')
+y = input('masukkan angka ke 2 :')
+num1 = int(x)
+num2 = int(y)
+print(num1, '+', num2, '=', num1 + num2)
+
+
+print ("selamat belajar semoga pintar")
+
+
